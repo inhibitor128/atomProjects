fa97938d98427c0611ff82e9edf47b30c9c79a4b
inhibitor128 <inhibitor128@gmail.com>
2021-06-26 17:13:54 -0400

    vv

--- java/eightBall/eightBall.java
+++ java/eightBall/eightBall.java
@@ -1,5 +1,7 @@
 public class eightBall {
+
   public static void main(String[] args) {
+
     String[] wordListOne = {"As I see it, yes", "Ask again later", "Better not tell you", "Cannot predict now", "Concentrate and ask again", "Don’t count on it", "It is certain", "It is decidedly so", "Most likely", "My reply is no", "My sources say no", "Outlook good", "Outlook not so good", "Reply hazy try again", "Signs point to yes", "Very doubtful", "Without a doubt", "Yes", "Yes, definitely", "You may rely on it"};
 
     int oneLength = wordListOne.length;
