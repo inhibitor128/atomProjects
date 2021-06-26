896a8ff0ad9e48315d0a56c1d219dc181a836b04
inhibitor128 <inhibitor128@gmail.com>
2021-06-26 17:33:18 -0400

    magic eightball

--- java/eightBall/eightBall.java
+++ java/eightBall/eightBall.java
@@ -2,10 +2,11 @@ public class eightBall {
 
   public static void main(String[] args) {
 
-    System.out.println("Ask your question ***Press enter to continue***");
+    System.out.println("Ask your Question");
+    System.out.println("Press enter to continue");
 
     try{System.in.read();}
-            catch(Exception e){}
+    catch(Exception e){}
 
     String[] wordListOne = {"As I see it, yes", "Ask again later", "Better not tell you", "Cannot predict now", "Concentrate and ask again", "Don’t count on it", "It is certain", "It is decidedly so", "Most likely", "My reply is no", "My sources say no", "Outlook good", "Outlook not so good", "Reply hazy try again", "Signs point to yes", "Very doubtful", "Without a doubt", "Yes", "Yes, definitely", "You may rely on it"};
 
