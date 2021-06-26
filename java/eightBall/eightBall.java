797d968bba98d42c60d9a81d87b0f5fffaf32b3c
inhibitor128 <inhibitor128@gmail.com>
2021-06-26 17:02:25 -0400

    Magic Eight Ball

---
+++ java/eightBall/eightBall.java
@@ -0,0 +1,14 @@
+public class eightBall {
+  public static void main(String[] args) {
+    String[] wordListOne = {"As I see it, yes", "Ask again later", "Better not tell you", "Cannot predict now", "Concentrate and ask again", "Don’t count on it", "It is certain", "It is decidedly so", "Most likely", "My reply is no", "My sources say no", "Outlook good", "Outlook not so good", "Reply hazy try again", "Signs point to yes", "Very doubtful", "Without a doubt", "Yes", "Yes, definitely", "You may rely on it"};
+
+    int oneLength = wordListOne.length;
+
+    int rand1 = (int) (Math.random() * oneLength);
+
+    String phase = wordListOne[rand1];
+
+    System.out.println(phase);
+
+  }
+}
