d85330acff5b17e7a7e8d0734bd1da2c1d935b47
inhibitor128 <inhibitor128@gmail.com>
2021-06-27 04:26:24 -0400

    minor update on commints

--- bashScripts/brave_browser.sh
+++ bashScripts/brave_browser.sh
@@ -2,26 +2,26 @@
 
 
 #######################################
-# Bash script to install Brave Browser on Ubuntu 
-# Written by @inhibitor128 
+# Bash script to install Brave Browser on Ubuntu
+# Written by @inhibitor128
 #######################################
 
 
-echo "########## Installing Brave Browser for Ubuntu ##########"
+echo "Installing Brave Browser for Ubuntu"
 read -p "Press enter to continue"
-echo "########## Allowing the use of repositories accessed via the HTTP Secure protocol ##########"
+echo "Allowing the use of repositories accessed via the HTTP Secure protocol"
 sudo apt install apt-transport-https curl
 read -p "Press enter to continue"
-echo "########## Installing security key ##########"
+echo "Installing security key"
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 read -p "Press enter to continue"
-echo "########## Instaling brave repository ##########"
+echo "Instaling brave repository"
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 read -p "Press enter to continue"
-echo "########## System update ##########"
+echo "System update"
 sudo apt update
 read -p "Press enter to continue"
-echo "########## Install Brave Browser ##########"
+echo "Install Brave Browser"
 sudo apt install brave-browser
 
 #######################################################
