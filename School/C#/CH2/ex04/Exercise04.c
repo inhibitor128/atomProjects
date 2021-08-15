d4e45d0b11a85a5a35321b3bb2a79f1fb75431ea
inhibitor128 <inhibitor128@gmail.com>
2021-08-15 12:15:39 -0400

    school

---
+++ School/C#/CH2/ex04/Exercise04.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void)
+{
+  int value1, value2, sum;
+
+  value1 = 87;
+  value2 = 15;
+  sum = value1 - value2;
+  printf("%i subtracted from %i is %i\n", value2, value1, sum);
+
+  return 0;
+}
