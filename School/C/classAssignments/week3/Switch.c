fd42faab0b65a08144e978cc97b6d3a189deaa3e
inhibitor128 <inhibitor128@gmail.com>
2021-09-22 08:33:14 -0400

    hw

--- School/C/classAssignments/week3/Switch.c
+++ School/C/classAssignments/week3/Switch.c
@@ -1,5 +1,5 @@
 /**********************Heading*********************************************************
-Name: Joseph Gumbakis
+Name: J G
 Dates: 9/22/2021
 Purpose:Write a program that will perform the following steps
 		1.  User enters an integer, 0 to 9  (5)
@@ -10,33 +10,43 @@ Purpose:Write a program that will perform the following steps
 
 int main()
 {
-    //declare variables
+    //initiate variables
     int choice;
-
+    //declare variables
     printf("Enter an integer bewteen 0 and 9: ");
     scanf("%i", &choice);
     //check statement
     switch(choice)
     {
       case 0: printf("Zero \n");
+              printf("End of program");
               break;
       case 1: printf("One \n");
+              printf("End of program");
               break;
       case 2: printf("Two \n");
+              printf("End of program");
               break;
       case 3: printf("Three \n");
+              printf("End of program");
               break;
       case 4: printf("Four \n");
+              printf("End of program");
               break;
       case 5: printf("Five \n");
+              printf("End of program");
               break;
       case 6: printf("six \n");
+              printf("End of program");
               break;
       case 7: printf("Seven \n");
+              printf("End of program");
               break;
       case 8: printf("Eight \n");
+              printf("End of program");
               break;
       case 9: printf("Nine \n");
+              printf("End of program");
               break;
       default: printf("Invalid Option \n");
                break;
