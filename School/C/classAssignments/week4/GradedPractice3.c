6d756cf7f7d78ddeb84dd9100145f2587a56010c
inhibitor128 <inhibitor128@gmail.com>
2021-09-29 10:45:42 -0400

    hw

--- School/C/classAssignments/week4/GradedPractice3.c
+++ School/C/classAssignments/week4/GradedPractice3.c
@@ -8,6 +8,26 @@ Program Description:  Write a menu-driven program that allow the user to pick a
 
 int main()
 {
+  //declare variables
+  int num;
 
-     return 0;
+  //display menue
+  printf ("Menue\n");
+  printf("1 - Shrek\n");
+  printf("2 - Babe\n");
+  printf("3 - Dumbo\n");
+  printf("4 - Exit Program\n");
+
+  printf("Enter your choice: ");
+  scanf("%i", &num);
+
+  if(num < 0 || num > 4)
+    printf("Error: Please enter a valid choice");
+    else
+      switch(num)
+      {
+        case 1:
+      }
+
+  return 0;
 }
